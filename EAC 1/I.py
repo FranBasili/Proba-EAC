9982dbe82aaf9265f95a5f8d297d1cdddf1002e2
Nicolás <nbustelow@gmail.com>
2022-04-09 21:18:17 -0300

    few updates

--- EAC 1/I.py
+++ EAC 1/I.py
@@ -6,15 +6,15 @@ data = pd.read_table("heart2022.txt", sep=' ', names=['sbp', 'ldl', 'famhist', '
                     dtype={'sbp': int, 'ldl': float, 'famhist': bool, 'tipo A': float, 'chd': bool})
 
 GrupoA=data[(data['chd'] == False) & (data['famhist'] == True)]     # No tiene enfermedad ni antecedentes 
-GrupoB=data[((data['chd'] == False) & (data['famhist'] == False)) | # No tiene enfermedad pero sí antecedentes
-            ((data['chd'] == True) & (data['famhist'] == True))]    # Tiene enfermedad pero no antecedentes
+GrupoB=data[((data['chd'] == True) & (data['famhist'] == True)) | # No tiene enfermedad pero sí antecedentes
+            ((data['chd'] == False) & (data['famhist'] == False))]    # Tiene enfermedad pero no antecedentes
 GrupoC=data[(data['chd'] == True) & (data['famhist'] == False)]     # Tiene antecedentes y la enfermedad
 
 for aspecto in ["sbp","ldl",'tipo A']:
     data = pd.concat([GrupoA[aspecto].rename("Grupo A"), GrupoB[aspecto].rename("Grupo B"), GrupoC[aspecto].rename("Grupo C")], 
                     ignore_index=False, axis=1)
     shape = data.shape              
-    hist=data.plot.hist(bins=math.ceil(math.log(shape[0])), alpha=0.25)
+    hist=data.plot.hist(bins=math.ceil(math.log(shape[0])), alpha=0.3)
     hist.set_ylabel("Frecuencia")
     hist.set_xlabel(aspecto)
     hist.plot()
